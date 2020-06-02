de2ae22e30ac814c96cb5b71a1ba1fe2c2e53d7e
Tyler Hawkins <tylerhawkins@workfront.com>
2020-06-01 21:01:03 -0600

    changes class name

--- src/root.component.js
+++ src/root.component.js
@@ -3,7 +3,7 @@ import "./root.component.css";
 
 export default function Root() {
   return (
-    <div className="container">
+    <div className="container1">
       <p>Page 1 App</p>
     </div>
   );
